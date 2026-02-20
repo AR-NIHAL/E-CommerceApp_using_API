d4e10d9c203a08169d86524bce940febef18a515
Ashikur Rahman Nihal <arnihal.cs@gmail.com>
2026-02-20 23:31:26 +0600

    Update section_title.dart

--- ecommerce_riverpod_api/lib/features/widgets/section_title.dart
+++ ecommerce_riverpod_api/lib/features/widgets/section_title.dart
@@ -3,7 +3,7 @@ import 'package:flutter/material.dart';
 class SectionTitle extends StatelessWidget {
   final String title;
   final VoidCallback onSeeAll;
-  const SectionTitle({super.key,required this.title, required this.onSeeAll});
+  const SectionTitle({super.key, required this.title, required this.onSeeAll});
 
   @override
   Widget build(BuildContext context) {
@@ -11,12 +11,15 @@ class SectionTitle extends StatelessWidget {
       padding: const EdgeInsets.symmetric(horizontal: 16),
       child: Row(
         children: [
-          Text(title, style: const TextStyle(fontSize: 16, fontWeight: FontWeight.bold)),
+          Text(
+            title,
+            style: const TextStyle(fontSize: 16, fontWeight: FontWeight.bold),
+          ),
           const Spacer(),
           GestureDetector(
             onTap: onSeeAll,
             child: const Text('See All', style: TextStyle(color: Colors.red)),
-          )
+          ),
         ],
       ),
     );
